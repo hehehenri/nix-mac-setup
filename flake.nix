e08c0f507a27628f6afe8057c6daf18904f3a0a4
henri <henri@henr.in>
2024-08-26 20:41:58 -0300

    add more packages and init devenv

--- flake.nix
+++ flake.nix
@@ -5,39 +5,30 @@
 
 	outputs = { self, nixpkgs }: {
 		packages."aarch64-darwin".default = 
-		let
+		let 
 			pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-			
-			# TODO: find a way to use wrangler and pnpm from nix, pinning them to a specific version
-			# pnpm = pkgs.nodePackages.pnpm.overrideAttrs (old: rec {
-			#   version = "9.3.0";
-			#   src = pkgs.fetchurl {
-			# 	url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-			# 	sha256 = "woooooo";
-			#   };
-			# });
-			#
-			# wrangler = pkgs.nodePackages.wrangler.overrideAttrs (old: rec {
-			#   version = "3.58.0";
-			#   src = pkgs.fetchurl {
-			# 	url = "https://registry.npmjs.org/wrangler/-/wrangler-${version}.tgz";
-			# 	sha256 = "sha512-bRe/y/LKjIgp3L2EHjc+CvoCzfHhf4aFTtOBkv2zW+VToNJ4KlXridndf7LvR9urfsFRRo9r4TXCssuKaU+ypQ==";
-			#   };
-			# });
-				
-		in pkgs.buildEnv {
+		in
+		pkgs.buildEnv {
 			name = "home-packages";
 			paths = with pkgs; [
-				# tools
+				# shell related
 				git
 				gh
+				tmux
+				devenv
+
+				# neovim and dependencies
 				neovim
 				ripgrep
-				bun
 
-				# languages
+				# languages and stuff
 				nodejs
+				bun
 				go
+
+				# macOS related
+				yabai
+				skhd
 			];
 		};
 	};
